87caf0dbf9de13ee48e0697f9843632c82b43a78
mamitiana <mamitianaignace@gmail.com>
2022-04-03 12:21:42 +0300

    intro gym

---
+++ introgym.py
@@ -0,0 +1,15 @@
+import gym
+
+env = gym.make("CartPole-v1")
+total_reward = 0.0
+total_steps = 0
+obs = env.reset()
+print("sssssssss")
+while True:
+    action = env.action_space.sample()
+    obs , reward , done , _ = env.step(action)
+    total_reward += reward
+    total_steps+=1
+    if done:
+        break
+print("Episode done in %d steps, total reward %.2f" % (total_steps, total_reward))
